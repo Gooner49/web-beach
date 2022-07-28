2add1156d944582ade8df5e466187cae2390cad1
Sankalan <sank.rrs88@gmail.com>
2022-07-29 01:32:43 +0530

    spring security get started

---
+++ demo1/src/main/java/com/example/demo/UserDtlSrvce.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+public class UserDtlSrvce implements UserDetailsService 
+{
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new MyUserPrincipal(user);
+    }
+    
+}
