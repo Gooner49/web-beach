ad34044e628def3a5610e259d832a32c4195adb3
Sankalan <sank.rrs88@gmail.com>
2023-08-10 13:31:58 +0530

    microservices v1

--- demo2/src/main/java/com/example/demo/DemoApplication.java
+++ demo2/src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableMongoRepositories (basePackageClasses =CustRepo.class)
 public class DemoApplication {
 
--- demo2/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo2/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,21 +1,17 @@
 package com.example.demo;
-
 import org.junit.jupiter.api.Test;
+//import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.runner.RunWith;
-//import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 //@RunWith(SpringRunner.class)
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = DemoApplication.class)
-class DemoApplicationTests {
-
+@SpringBootTest
+public class DemoApplicationTests 
+{
 	@Test
-	void contextLoads()
+	public void contextLoads()
 	 {
 
 	 }
-
-	
 }
